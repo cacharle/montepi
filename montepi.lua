615bad9c8d40e9f0a9574dad872b354cae176f87
Charles Cabergs <me@cacharle.xyz>
2021-11-26 22:20:26 +0100

    Added pi estimation, Drawing in/out of circle points, Drawing pi estimate string

--- montepi.lua
+++ montepi.lua
@@ -1,18 +1,39 @@
 local SDL = require "SDL"
+SDL.ttf = require "SDL.ttf"
 
-SDL.init { SDL.flags.Video }
+function unwrap(ret_err)
+    ret, err = ret_err
+    if not ret then
+        error(err)
+    end
+    return ret
+end
 
-local width  = 640
-local height = 480
+unwrap(SDL.init { SDL.flags.Video })
+unwrap(SDL.ttf.init())
 
-local window, err = SDL.createWindow {
+local width  = 600
+local height = width
+
+local window = unwrap(SDL.createWindow {
     title  = "TSP",
-    width  = 640,
-    height = 480,
-    flags  = SDL.window.Resizable,
-}
+    width  = width,
+    height = height,
+    -- flags  = SDL.window.Resizable,
+})
 
-local renderer, err = SDL.createRenderer(window, 0, {})
+local renderer = unwrap(SDL.createRenderer(window, 0, {}))
+
+local font = unwrap(SDL.ttf.open("/usr/share/fonts/TTF/FiraMono-Regular.ttf", 28))
+
+local points_in = {}
+local points_out = {}
+
+local pi = 3.14
+
+local last_time_pi_string = SDL.getTicks()
+local pi_string_draw_interval = 100
+local texture = nil
 
 local running = true
 while running do
@@ -23,13 +44,50 @@ while running do
             local code = e.keysym.scancode
             if code == SDL.scancode.Escape or code == SDL.scancode.Q then
                 running = false
-            elseif code == SDL.scancode.Space then
-                pause = not pause
             end
         end
     end
+    local point = { x = math.random() - 0.5, y = math.random() - 0.5 }
+    if math.sqrt(point.x * point.x + point.y * point.y) < 0.5 then
+        table.insert(points_in, point)
+    else
+        table.insert(points_out, point)
+    end
+    -- print(#points_in, #points_out)
+    -- print(#points_in / #points_out)
+    point.x = math.floor(point.x * width + width / 2)
+    point.y = math.floor(point.y * height + height / 2)
+    -- print(point.x, point.y)
 
     renderer:setDrawColor(0xff222222)
     renderer:clear()
+    renderer:setDrawColor(0xffff00ff)
+    renderer:drawPoints(points_in)
+    renderer:setDrawColor(0xff00ffff)
+    renderer:drawPoints(points_out)
+
+    local current_time = SDL.getTicks()
+    if current_time > last_time_pi_string + pi_string_draw_interval then
+        local pi_estimate = 4 * (#points_in / (#points_out + #points_in))
+        local surface = font:renderUtf8(string.format("%.20f", pi_estimate), "blended", { r = 255, g = 255, b = 255 })
+        texture = renderer:createTextureFromSurface(surface)
+        last_time_pi_string = current_time
+    end
+    local rect_w = 270
+    local rect_h = 40
+    local rect = {
+        x = width / 2 - rect_w / 2,
+        y = height / 2 - rect_h / 2,
+        w = rect_w,
+        h = rect_h,
+    }
+    if texture then
+        renderer:copy(texture, nil, rect)
+    end
+
     renderer:present()
+    SDL.delay(3)
 end
+
+SDL.ttf.quit()
+SDL.quit()
